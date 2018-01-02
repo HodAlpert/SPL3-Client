3c0790269f37a9139dfe18c9e63ce1fbc7cd04cd
omri <omrieitan@gmail.com>
2018-01-02 20:14:08 +0200

    code cleaning

--- Client/src/EchoClient.cpp
+++ Client/src/EchoClient.cpp
@@ -7,6 +7,7 @@
 /**
 * This code assumes that the server replies the exact text the client sent it (as opposed to the practical session example)
 */
+
 int main (int argc, char *argv[]) {
     if (argc < 3) {
         std::cerr << "Usage: " << argv[0] << " host port" << std::endl << std::endl;
@@ -14,9 +15,6 @@ int main (int argc, char *argv[]) {
     }
     std::string host = argv[1];
     short port = atoi(argv[2]);
-//    std::string host = "127.0.0.1";
-//    short port = 2000;
-
 
     ConnectionHandler connectionHandler(host, port);
     if (!connectionHandler.connect()) {
@@ -26,7 +24,6 @@ int main (int argc, char *argv[]) {
         std::cout << "connected to server" << std::endl;
     ClientWriter writer(connectionHandler);
     boost::thread WriterThread(writer);
-
 
     //From here we will see the rest of the ehco client implementation:
     while (true) {
