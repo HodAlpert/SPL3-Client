276a0751505494428dfd4652d2b3704948c80d72
omri <omrieitan@gmail.com>
2018-01-02 10:19:56 +0200

    bla

--- Client/src/ClientListener.cpp
+++ Client/src/ClientListener.cpp
@@ -14,6 +14,17 @@ public:
         std::string answer;
         while(_handler.getLine(answer))
         boost::this_thread::yield(); //Gives up the remainder of the current thread's time slice, to allow other threads to run. 
+        // Get back an answer: by using the expected number of bytes (len bytes + newline delimiter)
+        // We could also use: connectionHandler.getline(answer) and then get the answer without the newline char at the end
+        len=answer.length();
+        // A C string must end with a 0 char delimiter.  When we filled the answer buffer from the socket
+        // we filled up to the \n char - we must make sure now that a 0 char is also present. So we truncate last character.
+        answer.resize(len-1);
+        std::cout << "Reply: " << answer << " " << len << " bytes " << std::endl << std::endl;
+        if (answer == "bye") {
+            std::cout << "Exiting...\n" << std::endl;
+            break;
+        }
     }
 };
  
