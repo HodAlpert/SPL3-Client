a58464d03403e73c2187958beaac140f96c42499
hod <hod246>
2018-01-02 10:53:50 +0200

    change name

--- Client/src/EchoClient.cpp
+++ Client/src/EchoClient.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include "../include/ConnectionHandler.h"
 #include <boost/thread/thread.hpp>
+#include "./ClientWriter.cpp"
 
 /**
 * This code assumes that the server replies the exact text the client sent it (as opposed to the practical session example)
@@ -17,42 +18,35 @@ int main (int argc, char *argv[]) {
     short port = 2000;
 
 
-
     ConnectionHandler connectionHandler(host, port);
     if (!connectionHandler.connect()) {
         std::cerr << "Cannot connect to " << host << ":" << port << std::endl;
         return 1;
-    }
-    else
-        std::cout<<"connected to server"<<std::endl;
+    } else
+        std::cout << "connected to server" << std::endl;
+    ClientWriter writer(connectionHandler);
+    boost::thread WriterThread(writer);
 
 
     //From here we will see the rest of the ehco client implementation:
     while (true) {
-//        try {//checks if thread was interrupted
-//            boost::this_thread::interruption_point();
-//        }
-//        catch (boost::thread_interrupted&) {//
-//            std::cout << "Disconnected. Exiting...\n" << std::endl;
-//            break;
-//        }
-        const short bufsize = 1024;
-        char buf[bufsize];
-        std::cin.getline(buf, bufsize);
-        std::string line(buf);
-        int len=line.length();
-        if (!connectionHandler.sendLine(line)) {
+        std::string answer;
+        size_t len;
+        if (!connectionHandler.getLine(answer)) {
             std::cout << "Disconnected. Exiting...\n" << std::endl;
             break;
         }
-        // connectionHandler.sendLine(line) appends '\n' to the message. Therefor we send len+1 bytes.
-        std::cout << "Sent " << len+1 << " bytes to server" << std::endl;
-
-
-        // We can use one of three options to read data from the server:
-        // 1. Read a fixed number of characters
-        // 2. Read a line (up to the newline character using the getline() buffered reader
-        // 3. Read up to the null character
+        len = answer.length();
+        // A C string must end with a 0 char delimiter.  When we filled the answer buffer from the socket
+        // we filled up to the \n char - we must make sure now that a 0 char is also present. So we truncate last character.
+        answer.resize(len - 1);
+        std::cout << "Reply: " << answer << " " << len << " bytes " << std::endl << std::endl;
+        if (answer == "bye") {
+            std::cout << "Exiting...\n" << std::endl;
+            WriterThread.boost::interrupt();
+            connectionHandler.set_shouldTerminate(true);
+            break;
+        }
 
     }
     return 0;
