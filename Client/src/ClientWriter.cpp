83b809432e3e7f60984a683bf315bfab45006f10
omri <omrieitan@gmail.com>
2018-01-02 10:40:36 +0200

    writer update

--- Client/src/ClientWriter.cpp
+++ Client/src/ClientWriter.cpp
@@ -11,20 +11,23 @@ public:
     ClientWriter (ConnectionHandler& handler) : _handler(handler),_shouldTerminate(false) {}
 
     void operator()(){
-        std::string answer;
-        while(_handler.getLine(answer))
-        boost::this_thread::yield(); //Gives up the remainder of the current thread's time slice, to allow other threads to run. 
-        // Get back an answer: by using the expected number of bytes (len bytes + newline delimiter)
-        // We could also use: connectionHandler.getline(answer) and then get the answer without the newline char at the end
-        len=answer.length();
-        // A C string must end with a 0 char delimiter.  When we filled the answer buffer from the socket
-        // we filled up to the \n char - we must make sure now that a 0 char is also present. So we truncate last character.
-        answer.resize(len-1);
-        std::cout << "Reply: " << answer << " " << len << " bytes " << std::endl << std::endl;
-        if (answer == "bye") {
-            std::cout << "Exiting...\n" << std::endl;
-            break;
-        }
-    }
+        while (!_handler.shouldTerminate()) {
+            try {//checks if thread was interrupted
+                boost::this_thread::interruption_point();
+                const short bufsize = 1024;
+                char buf[bufsize];
+                std::cin.getline(buf, bufsize);
+                std::string line(buf);
+                size_t len=line.length();
+                if (!_handler.sendLine(line)) {
+                    std::cout << "Disconnected. Exiting...\n" << std::endl;
+                    break;
+                }
+                std::cout << "Sent " << len+1 << " bytes to server" << std::endl;
+            }
+            catch (boost::thread_interrupted&) {//
+                std::cout << "Disconnected. Exiting...\n" << std::endl;
+                break;
+            }
 };
  
